014004ed383004c3882210aa9953818cd6763ce2
Brett Roper <roper.brett@gmail.com>
2016-06-30 06:36:56 +0800

    parse filenames from sql directory

--- data/create-sqlite.php
+++ data/create-sqlite.php
@@ -5,21 +5,37 @@ header("Expires: Tue, 03 Jul 2001 06:00:00 GMT");
 $pdo = new PDO('sqlite:travellerct.sqlite3');
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-// create our tables and insert data if table empty
-$sql = file_get_contents('sql/create_character.sql');
-$result = $pdo->exec($sql);
+// create tables based on code in files named create_* in the sql directory
+echo 'Creating database objects.<br>';
+$filenames = glob ('sql/create_*');
+if ($filenames) {
+    // scripts exist, execute them
+    foreach ($filenames as $filename) {
+        try {
+            // drop the table
+            $sql = file_get_contents($filename);
+            $pdo->exec($sql);
+            echo 'Script '.$filename.' executed.<br>'.PHP_EOL;
+        } catch (Exception $e) {
+            // report error keep going
+            echo 'Script '.$filename.' not executed: '.$e->getMessage().'<br>'.PHP_EOL;
+        }
+    }
+} else {
+    echo 'No create scripts found. No database objects created.<br>'.PHP_EOL;
+}
 
-// prepare the data and insert it
+// optionally insert sample data
 if (isset($_GET['sampledata']) && $_GET['sampledata'] == 'yes') {
-    print 'Insering sample data.<br>';
+    echo 'Insering sample data.<br>';
 
     $sql = file_get_contents('sql/insert_character.sql');
     $result = $pdo->exec($sql);
-    print $result.' rows inserted into table character.<br>';
+    echo $result.' rows inserted into table character.<br>';
 }
 
 // display the tables
-print 'All tables in database:<br>';
+echo 'List all tables in database:<br>';
 foreach($pdo->query("select name from sqlite_master where type='table'") as $row) {
-    print htmlentities($row['name']).'<br>';
+    echo htmlentities($row['name']).'<br>';
 }

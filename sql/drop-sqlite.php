faa97c9b303cc2d7250310c12b76b69e874ab71b
Brett Roper <roper.brett@gmail.com>
2016-06-30 04:38:30 +0800

    Iterate over all tables in drop script.

--- sql/drop-sqlite.php
+++ sql/drop-sqlite.php
@@ -1,6 +1,25 @@
 <?php
+// prevent caching
+header("Expires: Tue, 03 Jul 2001 06:00:00 GMT");
+
 $pdo = new PDO('sqlite:travellerct.sqlite3');
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-// drop our tables
-if (! $pdo->exec('drop table character')) { echo 'table character dropped';}
+// drop all tables
+$tables = $pdo->query("select name from sqlite_master where type='table'")->fetchAll(PDO::FETCH_ASSOC);
+if ($tables) {
+    // tables exist, drop them
+    foreach($tables as $row) {
+        try {
+            // drop the table
+            $pdo->exec('drop table '.$row['name']);
+            echo 'Table '.$row['name'].' dropped.';
+        } catch (Exception $e) {
+            // report error keep dropping
+            echo 'Table '.$row['name'].' not dropped: ';
+            echo $e->getMessage();
+        }
+    }
+} else {
+    echo 'No tables exist to drop.';
+}

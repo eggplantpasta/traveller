e45b34b82f6caf48a0d30969aa8a6c017925d1b0
Brett Roper <roper.brett@gmail.com>
2016-08-16 17:23:18 +0800

    create app framework

---
+++ src/public/index.php
@@ -0,0 +1,29 @@
+<?php
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+$config['displayErrorDetails'] = true;
+$config['addContentLengthHeader'] = false;
+
+$config['db']['host']   = "localhost";
+$config['db']['user']   = "user";
+$config['db']['pass']   = "password";
+$config['db']['dbname'] = "exampleapp";
+
+require '../vendor/autoload.php';
+
+spl_autoload_register(function ($classname) {
+    require ("../classes/" . $classname . ".php");
+});
+
+$app = new \Slim\App(["settings" => $config]);
+
+$container = $app->getContainer();
+
+$app->get('/hello/{name}', function (Request $request, Response $response) {
+    $name = $request->getAttribute('name');
+    $response->getBody()->write("Hello, $name");
+
+    return $response;
+});
+$app->run();
